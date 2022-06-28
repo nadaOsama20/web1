d3ceb41b35be4c40c72fa28cbdd92e39f599f41a
nadaOsama20 <101597758+nadaOsama20@users.noreply.github.com>
2022-06-28 15:43:05 +0300

    Add files via upload

---
+++ food script.js
@@ -0,0 +1,18 @@
+
+function add1(){
+    var add = document.getElementById("ad1").value;
+    alert("تم اشتراك صاحب الايميل " + add )
+}
+function add2(){
+    var add1 = document.getElementById("ad2").value;
+    var add2 = document.getElementById("ad3").value;
+    var add3 = document.getElementById("ad3").value;
+    confirm("هل فعلا تريد الطلب ؟")
+    alert(" \nالاسم : " + add1+" \nالبريد الالكتروني : "  +add2 + " \nالرسالة : "+  add3)
+}
+//dsklm
+/*ننمم*/ 
+function free(){
+
+}
+
